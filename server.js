2d7cb7fa8a1aa65b64b9e7e5b7751d8605225061
Kamal <ks708570@gmail.com>
2023-12-23 00:18:36 +0530

    error handling

--- server.js
+++ server.js
@@ -1,66 +1,69 @@
-const express = require('express');
-const cors = require('cors');
-const fetch =  require('cross-fetch');
+const express = require("express");
+const cors = require("cors");
+const fetch = require("cross-fetch");
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 
+app.get("/api/restaurants", (req, res) => {
+  const { lat, lng } = req.query;
+  console.log(req.query);
 
-app.get('/api/restaurants', (req,res)=>{
-    const {lat, lng} = req.query;
-    console.log(req.query);
+  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-
-    fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-        }
-    }).then(response=>{
-        if(!response.ok){
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    }).then(data=>{
-        console.log(data);
-        res.json(data);
-    }).catch(err=>{
-        console.log(err);
-        res.status(500).send(err);
+  fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.error("Error:", response.status, response.statusText);
+        return Promise.reject("Network response was not ok");
+      }
+      return response.json();
     })
-})
-app.get('/api/menu', (req,res)=>{
-    const {lat, lng, restaurantId} = req.query;
-    console.log(req.query);
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      res.status(500).send("An error occurred");
+    });
+});
+app.get("/api/menu", (req, res) => {
+  const { lat, lng, restaurantId } = req.query;
+  console.log(req.query);
 
-    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}&submitAction=ENTER`;
+  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}&submitAction=ENTER`;
 
-    fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-        }
-    }).then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        res.json(data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('An error occurred');
-      });
-  });
-  
+  fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
